0abf9b92a7130ff0b0323487eb4e52a048bb72d5
Yuwono Bangun Nagoro <morpheusgun@outlook.com>
2016-04-19 20:53:26 +0700

    Create Hello World.fs

---
+++ Hello World.fs
@@ -0,0 +1,8 @@
+// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System
+[<EntryPoint>]
+let main argv = 
+    printfn "Hello World!\n"
+    Console.ReadLine()
+    0 // return an integer exit code
